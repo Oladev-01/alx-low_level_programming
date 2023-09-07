7ebd5b064d64871909c8654f3cbd0cced8e9d4f4
olalekan <lekanmojibola@gmail.com>
2023-09-07 23:53:29 +0000

    This is a file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this function is one that returns output to standard error
+ *
+ * Return: always return 1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+
+}
