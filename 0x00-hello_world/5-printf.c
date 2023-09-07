858979f5f109b574f64a3534c4080c2f49914a0e
olalekan <lekanmojibola@gmail.com>
2023-09-07 22:12:59 +0000

    This is a file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * Return: always retun 0
  */
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
