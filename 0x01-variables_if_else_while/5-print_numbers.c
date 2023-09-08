93f161f2a54eed05a6957c88fb73daf695f66fe9
olalekan <lekanmojibola@gmail.com>
2023-09-08 22:27:32 +0000

    this is a file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints the numbers in base 10 starting -
+ * from 0
+ *
+ * Return: this should return 0 for success
+ */
+
+int main(void)
+
+{
+
+	int i = 0;
+
+	if (i < 10)
+
+	{
+		printf("%d", i);
+
+		i++;
+
+	}
+
+	else
+
+	{
+		printf("0");
+
+	}
+
+	return (0);
+
+}
