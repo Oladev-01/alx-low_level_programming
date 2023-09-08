65ada22553bfc753754ac324ad3e5f575d47e106
olalekan <lekanmojibola@gmail.com>
2023-09-08 00:44:20 +0000

    This is a file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - this function is one that returns output to standard error
  *
- * Return: always return 1
+ * Return: always return 1i
  */
 int main(void)
 {
-	 const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	 write(2, message, 59);
-	     return (1);
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, message, 59);
+	return (1);
 
 }
