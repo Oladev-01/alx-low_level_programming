653645d2a562bd7851e1a6e9ad3aea1d610bc31d
olalekan <lekanmojibola@gmail.com>
2023-09-08 21:28:04 +0000

    this is a file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * maim - this function is for printing alphabets
+ *
+ * Return: will return 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 'a';
+
+	while(i <= 'z')
+
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
