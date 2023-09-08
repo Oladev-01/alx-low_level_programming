3ef74ce626c5151cda820ec627ec744ff75e521a
olalekan <lekanmojibola@gmail.com>
2023-09-08 00:18:40 +0000

    This is a file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - this function is one that returns output to standard error
  *
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+	 const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; write(2, message, 59);
+	     return (1);
 
 }
