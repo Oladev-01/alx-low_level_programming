e8dba2672b885c4e8cfa381d1fe73dd2648b7fe6
olalekan <lekanmojibola@gmail.com>
2023-09-12 21:04:39 +0000

    This is a file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-  *void print_alphabet_x10(void) - this is the entry point of the code
-  * this program prints 10 times the alphabet, in lowercase, followed by a new line
+  *print_alphabet_x10 - this is the entry point of the code
+  * this program prints 10 times the alphabet,-
+  *in lowercase, followed by a new line
   *
   *Return: this should return 0 for success
-  */
+ */
  void print_alphabet_x10(void)
 {
 	int me = 0;
