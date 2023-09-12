224044cc61c6ab1f1b918fcaaa687803cbee6e21
olalekan <lekanmojibola@gmail.com>
2023-09-12 15:54:47 +0000

    This is a file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * main - this program prints a statement
+ *
+ * return: this should return 1 for succcess
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (1);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This file is for functions and nested loop
