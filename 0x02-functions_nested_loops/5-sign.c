7f81cac50cd351c13cd45d4e66160b3be984ff3c
olalekan <lekanmojibola@gmail.com>
2023-09-12 23:40:55 +0000

    This is a file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,25 +10,20 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
-		_putchar(43);
 	}
 
 	else if (n == 0)
 	{
+		_putchar(0);
 		return (0);
-		_putchar(48);
 	}
 
 	else if (n < 0)
 	{
+		_putchar('-');
 		return (-1);
-		_putchar(45);
-	}
-
-	else
-	{
-		_putchar(64);
 	}
 
 	return (0);
