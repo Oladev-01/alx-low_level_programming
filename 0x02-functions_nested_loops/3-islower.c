9ad247aa7de11a9bcacf3fefcbacfd3f07b5ee65
olalekan <lekanmojibola@gmail.com>
2023-09-12 21:57:31 +0000

    This is a file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_islower - this is the entry point
+ *
+ *Return: this should return 0 for sucess
+ */
+int _islower(int c)
+{
+
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+		else
+		{
+
+			return (0);
+		}
+
+	return (0);
+}
+
