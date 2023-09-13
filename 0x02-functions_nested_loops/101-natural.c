0c9d626ce399702a249b963fcebf6a18ef33112f
olalekan <lekanmojibola@gmail.com>
2023-09-13 23:21:00 +0000

    This is a file

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,22 @@
-#include "main.h"
+#include <stdio.h>
 /**
- *
- *
- *
- *
- *
+ *main - entry
+ *Return: 0
+ */
+int main(void)
+{
+	int a, b;
+
+	z = 0;
+
+	while (a < 1024)
+	{
+	if ((a % 3 == 0) || (a % 5 == 0))
+	{
+	b = b + a;
+	}
+	a++;
+	}
+	printf("%d\n",  b);
+	return (0);
+}
