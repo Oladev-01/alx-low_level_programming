ac41ebd5e57674d4a45d990666557b80b292480c
olalekan <lekanmojibola@gmail.com>
2023-09-13 00:26:12 +0000

    This is a file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_abs - this is my entry point
+ *
+ *@j: this is my declared parameter
+ *
+ *Return: returns 0 for success
+ */
+int _abs(int j)
+{
+
+	if (j < 0)
+	{
+		int lekan = j * -1;
+
+		return (lekan);
+	}
+
+	return (0);
+}
+
+
