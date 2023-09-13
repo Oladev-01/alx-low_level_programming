82d6a285f9a7c4ac289f97b7a4fe87e93913fcda
olalekan <lekanmojibola@gmail.com>
2023-09-13 23:12:38 +0000

    This is a file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,9 +9,9 @@ void print_times_table(int n)
 {
 	int a, b, c;
 
-	if (a >= 0 && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
-	for (n = 0; n <= a; b++)
+	for (a = 0; a <= n; a++)
 	{
 	_putchar('0');
 	for (b = 1; b <= n; b++)
@@ -25,14 +25,14 @@ void print_times_table(int n)
 	_putchar(' ');
 	if (c >= 100)
 	{
-	_putchar((b / 100) + '0');
-	_putchar(((b / 10)) % 10 + '0');
+	_putchar((c / 100) + '0');
+	_putchar(((c / 10)) % 10 + '0');
 	}
-	else if (b <= 99 && b >= 10)
+	else if (c <= 99 && c >= 10)
 	{
-	_putchar((b / 10) + '0');
+	_putchar((c / 10) + '0');
 	}
-	_putchar((b % 10) + '0');
+	_putchar((c % 10) + '0');
 	}
 	_putchar('\n');
 	}
