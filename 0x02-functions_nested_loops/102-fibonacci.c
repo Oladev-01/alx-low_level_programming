f9d9e5f5c139c8b926c558182d804fe2ca8ee6b9
olalekan <lekanmojibola@gmail.com>
2023-09-13 23:33:42 +0000

    This is a file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,27 @@
-#include "main.h"
+#include <stdio.h>
 /**
- *
- *
- *
- *
- *
+ *main - entry
+ *Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+	long j = 1, c = 2;
+
+	while (a < 50)
+	{
+	if (a == 0)
+	printf("%ld", j);
+	else if (a == 1)
+	printf(", %ld", c);
+	else
+	{
+	c = c + j;
+	j = c - j;
+	printf(", %ld", c);
+	}
+	a++;
+	}
+	printf("\n");
+	return (0);
+}
