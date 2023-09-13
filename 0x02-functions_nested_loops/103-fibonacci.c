f17b023f0db4a849e1741b9dce97bb15ce3fe2c5
olalekan <lekanmojibola@gmail.com>
2023-09-13 23:43:46 +0000

    This is a file

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 /**
+ *main - entry
+ *less than 4,000,000
  *
- *
- *
- *
- *
+ *Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+	long b = 1, c = 2, plus = c;
+
+	while (c + b < 4000000)
+	{
+	c = c + b;
+	if (c % 2 == 0)
+	plus = plus + c;
+	b = c - b;
+	a++;
+	}
+	printf("%ld\n", plus);
+	return (0);
+}
