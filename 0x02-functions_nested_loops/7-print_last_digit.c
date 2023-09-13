4291183130bb10687a263f115bcbf1092032b458
olalekan <lekanmojibola@gmail.com>
2023-09-13 01:34:46 +0000

    This is a file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_last_digit - this is the entry point
+ *
+ *@f: this is my declared variable
+ *
+ *Return: returns 0 for success
+ */
+int print_last_digit(int f)
+{
+	int g;
+
+	g = f % 10;
+	if (f > 0)
+	{
+		_putchar(g + '0');
+	}
+	else
+	{
+		int r
+		r = -g
+			_putchar(r +0');
+	}
+	return (0);
+}
+
