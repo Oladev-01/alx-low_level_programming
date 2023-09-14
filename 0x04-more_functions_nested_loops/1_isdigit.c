fed9184142bb038f3450831757c1e5667d6f2b91
olalekan <lekanmojibola@gmail.com>
2023-09-14 14:28:17 +0000

    This is a file

--- 0x04-more_functions_nested_loops/1_isdigit.c
+++ 0x04-more_functions_nested_loops/1_isdigit.c
@@ -4,14 +4,12 @@
  *@c: it's a digit
  *Return: return 1 or 0
  */
-
 int _isdigit(int c)
 {
-   int c;
-   
-   c = 0;
-   if (c <= 9)
-    return (1);
-    else
-  return 0;
+	if (c >= 0 && c <= 9)
+	{
+	return (1);
+	}
+	else
+	return (0);
 }
