b2fc4ce13fd58736140ce88ff597e91cbea2bdfd
olalekan <lekanmojibola@gmail.com>
2023-09-14 15:22:10 +0000

    This is a file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,15 +4,14 @@
  *
  *Return: return 0
  */
- void print_most_numbers(void) 
+void print_most_numbers(void)
 {
-  int a;
-  
-  for (a = 0; a <= 9; a++)
-  {
-    if (a != 2 && a != 4)
-    putchar(a + '0');
-  }
-  putchar('\n');
-  return 0;
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	if (a != 2 && a != 4)
+	_putchar(a + '0');
+	}
+	_putchar('\n');
 }
