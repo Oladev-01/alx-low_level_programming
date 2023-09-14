f832c294cbb54648b95707fde103f8e7e6162ae6
olalekan <lekanmojibola@gmail.com>
2023-09-14 18:53:50 +0000

    This is a file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 /**
  *main - entry point
  *
- *Return: return null for success
+ *Return: return 0 for success
  */
 int main(void)
 {
@@ -11,21 +11,25 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-	if (a % 3 == 0 && a % 5 == 0)
+	if ((a % 3 == 0) && (a % 5 == 0))
 	{
-		printf("FizzBuzz ");
+		printf("FizzBuzz");
 	}
 	else if (a % 3== 0)
 	{
-		printf("Fizz ");
+		printf("Fizz");
 	}
 	else if (a % 5 == 0)
 	{
-		printf("Buzz ");
+		printf("Buzz");
 	}
 	else
 	{
-		printf("%d ", a);
+		printf("%d", a);
+	}
+	if (a != 100)
+	{
+		printf(" ");
 	}
 	}
 	printf("\n");
