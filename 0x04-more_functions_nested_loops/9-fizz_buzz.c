98f083f52c4c40d98bbf28967cd0b52896116835
olalekan <lekanmojibola@gmail.com>
2023-09-14 18:40:49 +0000

    This is a file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,13 @@ int main(void)
 	{
 		printf("FizzBuzz ");
 	}
-	else if (a % 3 == 0)
-	{
-		printf("Fizz ");
-	}
 	else if (a % 5 == 0)
 	{
 		printf("Buzz ");
+	}
+	else if (a % 3 == 0)
+	{
+		printf("Fizz ");
 	}
 	else
 	{
@@ -28,5 +28,4 @@ int main(void)
 	}
 	}
 	printf("\n");
-	return (0);
 }
