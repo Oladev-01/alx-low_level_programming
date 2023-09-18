90fc88aa63e4c501bac9be564e5e04612e95a28d
olalekan <lekanmojibola@gmail.com>
2023-09-18 18:35:56 +0000

    This is a file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *rev_string - entry
+ *@s: string
+ *Return: 0
+ */
+void rev_string(char *s)
+{
+	char a = s[0];
+	int b = 0;
+	int c;
+
+	while (s[b] != '\0')
+		b++;
+	for (c = 0; c < b; c++)
+	{
+		b--;
+		a = s[c];
+		s[c] = s[b];
+		s[b] = a;
+	}
+}
