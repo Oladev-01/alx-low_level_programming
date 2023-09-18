61af357b8264146bc483aa46319199c82a2a9794
olalekan <lekanmojibola@gmail.com>
2023-09-18 19:11:44 +0000

    This is a file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *char *_strcpy - entry
+ *@dest: copy
+ *@src: copied
+ *Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (*(src + 1) != '\0')
+	{
+		a++;
+	}
+	for (; b < 1; b++)
+	{
+		dest[b] = src[b];
+	}
+	dest[a] = '\0';
+	return (dest);
+}
+
