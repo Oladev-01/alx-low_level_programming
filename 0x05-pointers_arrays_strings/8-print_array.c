7b7bdb0a1008d53c89a87a2a6a6087adfa8b9bb2
olalekan <lekanmojibola@gmail.com>
2023-09-18 18:57:17 +0000

    This is a file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_array - entry
+ *@a: string
+ *@n: number
+ *Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; a < (n - 1); b++)
+	{
+		printf("%d, ", a[b]);
+	}
+		if (b == (n - 1))
+		{
+			printf("%d, a[n - 1]");
+		}
+		printf('\n');
+}
+
