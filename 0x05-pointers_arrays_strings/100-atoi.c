16aad08eb6f4e95bad20fa609f83782fe17e7115
oladev01 <lekanmojibola@gmail.com>
2023-09-24 21:18:57 +0100

    this is a commit branch for task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *_atoi - entry function
+ *@s: string to be printed
+ *Return: returns the integer from the string
+ */
+int _atoi(char *s)
+{
+	int a, sign = 1, space = 1, integer = 0;
+	unsigned int result = 0;
+
+	for (a = 0; *s != '\0'; a++)
+	{
+		if (s[a] == 32)
+		{
+			space *= 1;
+		}
+		else if (s[a] == 45)
+		{
+			sign *= -1;
+		}
+		else if (s[a] >= 48 && s[a] <= 57)
+		{
+			integer = 1;
+			result = (result * 10) + (s[a] - '0');
+		}
+		else if (integer == 1)
+		{
+			break;
+		}
+	}
+	return (result * sign);
+}
