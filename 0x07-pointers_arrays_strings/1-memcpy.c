e1e369d18e5a1dd1c00009f640482e71e76f96fe
oladev01 <lekanmojibola@gmail.com>
2023-09-25 18:42:47 +0100

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_memcpy - entry point
+ *@dest: destination to be copied to
+ *@src: copying from
+ *@n: number of bytes to copy
+ *Return: dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int block = 0;
+
+	while (block < n)
+	{
+		dest[block] = src[block];
+		block++;
+	}
+	dest[block] = '\0';
+	return (dest);
+}
