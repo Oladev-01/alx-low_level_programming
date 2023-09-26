c0f638b24ad3fb2eece67c653a4ba96b41aa68b0
oladev01 <lekanmojibola@gmail.com>
2023-09-26 16:37:27 +0100

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,29 +6,38 @@
  */
 int main(void)
 {
-	int a = 1;
-	int b = 2;
-	unsigned int hold;
-	int count = 2;
+	int a;
+	unsigned long b = 0, c = 1, sum, d, e, f, g, h, i;
 
-	printf("%d, %d, ", a, b);
-	while (count < 98)
+	for (a = 0; a < 92; a++)
 	{
-		hold = a + b;
-
-		if (count == 97)
-		{
-			printf("%u\n", hold);
-		}
-		else
-		{
-			printf("%u, ", hold);
-		}
-
-		a = b;
-		b = hold;
-		count++;
+		sum = b + c;
+		printf("%lu, ", sum);
+		b = c;
+		c = sum;
 	}
-
+	d = b / 10000000000;
+	f = c / 10000000000;
+	e = b % 10000000000;
+	g = c % 10000000000;
+	for (a = 93; a < 99; a++)
+	{
+		h = d + f;
+		;
+		i = e + g;
+		if (e + g > 9999999999)
+		{
+			h += 1;
+			i %= 10000000000;
+		}
+		printf("%lu%lu", h, i);
+		if (a != 98)
+			printf(", ");
+		d = f;
+		e = g;
+		f = h;
+		g = i;
+	}
+	printf("\n");
 	return (0);
 }
