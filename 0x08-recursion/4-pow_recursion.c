d32cb4e17bb7f739a2f909bcc8d8f138aa4acf02
oladev01 <lekanmojibola@gmail.com>
2023-09-28 18:20:49 +0100

    This is a project file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_pow_recursion - entry function
+ *@x: base
+ *@y: power
+ *Return: value of x to the power of y
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	return (-1);
+	else if (y == 0)
+	return (1);
+	else
+	return (x * _pow_recursion(x, y - 1));
+}
