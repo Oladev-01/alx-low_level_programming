8c4b9267288db759f283f21fdc1cccfc08d86b0e
oladev01 <lekanmojibola@gmail.com>
2023-09-28 16:58:37 +0100

    This is a project file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_puts_recursion - entry function for code
+ *@s: string to print
+ * Return: null
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[i]);
+	_puts(s + 1);
+}
