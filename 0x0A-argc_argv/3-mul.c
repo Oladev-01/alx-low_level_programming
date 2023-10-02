6bc53f07c6ab22f757708354d4207fd502c3f685
oladev01 <lekanmojibola@gmail.com>
2023-10-02 18:57:52 +0100

    This is a project file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this program prints the name of the argument passed to it
+ * @argc: argument counter
+ * @argv: argument name
+ * Return: 0 for sucess
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int mul = 1;
+
+	if (argc >= 1)
+	{
+		for (a = 1; a < argc; a++)
+		{
+			mul *= atoi(argv[a]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}
