dc25cf876078b12d22e0ce568b3dedfbccb64e6d
oladev01 <lekanmojibola@gmail.com>
2023-10-02 19:16:09 +0100

    This is a project file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,18 +11,18 @@ int main(int argc, char *argv[])
 	int a;
 	int mul = 1;
 
-	if (argc > 1)
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (argc > 1)
 	{
 		for (a = 1; a < argc; a++)
 		{
 			mul *= atoi(argv[a]);
 		}
 		printf("%d\n", mul);
-		return (0);
 	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
+	return (0);
 }
