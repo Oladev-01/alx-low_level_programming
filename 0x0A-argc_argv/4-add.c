6c9e65a0a7c8ccf2e7dff53639ad76aff9ff9b10
oladev01 <lekanmojibola@gmail.com>
2023-10-02 20:45:04 +0100

    This is a project file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,26 +8,28 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, sum = 0;
-	int n;
+	int sum = 0;
+	int i;
 
-	if (argc > 1)
-	{
-		for (a = 1; a < argc; a++)
-		{
-			n = atoi(argv[a]);
-			if (n < 0)
-			{
-				printf("Error\n");
-				return (1);
-			}
-			sum += n;
-		}
-		printf("%d\n", sum);
-	}
-	else if (argc < 2)
+	if (argc < 2)
 	{
 		printf("0\n");
+		return (0);
 	}
+
+	for (i = 1; i < argc; i++)
+	{
+		char *endptr;
+		long num = strtol(argv[i], &endptr, 10);
+
+		if (*endptr != '\0' || num < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += (int)num;
+	}
+
+	printf("%d\n", sum);
 	return (0);
 }
