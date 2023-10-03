2f4b33b9dd706da14e634749a6b9d07c17993da5
oladev01 <lekanmojibola@gmail.com>
2023-10-03 19:12:00 +0100

    This is a project file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - this function retrurns a pointer to a char
+ *  array and copying the string of the array in the parameter
+ * to the memory allocated for the declared char array
+ * @str: string to be copied, i.e the string in the parameter
+ * Return: should return the string pointed with the
+ * content of string in parameter
+ */
+char *_strdup(char *str)
+{
+	int len = strlen(str);
+	int a = 0, b = 0;
+	char *s;
+
+	s = (char *)malloc(len * sizeof(*str));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (a < len)
+	{
+		s[a] = str[a];
+		a++;
+	}
+	s[a] = '\0';
+	return (s);
+}
