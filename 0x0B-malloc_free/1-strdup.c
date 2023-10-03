8f23a2ccd35999024d99985f24ddf2915decb3a5
oladev01 <lekanmojibola@gmail.com>
2023-10-03 19:23:07 +0100

    This is a project file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 	char *s;
 
 	s = (char *)malloc((len + 1) * sizeof(*str));
-	if ((str == NULL || s == NULL)||(( s == NULL && str == NULL)))
+	if (s == NULL && str == NULL)
 	{
 		return (NULL);
 	}
