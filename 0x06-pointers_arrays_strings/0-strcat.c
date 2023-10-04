e0ea5130026440a2f2ab1a3f7b88fbd10f9387a0
oladev01 <lekanmojibola@gmail.com>
2023-10-04 17:54:29 +0100

    This is a project file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - this function concatenates a string to another string
+ * @dest: destination string to be concatenated into
+ * @src: source string to concatenate from
+ * Return: should return the string copy of dest and src
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *cat;
+	int a, b, len_dest, len_src;
+
+	len_dest = strlen(dest);
+	len_src = strlen(src);
+	for (a = 0; a < len_dest; a++)
+	{
+		cat[a] = dest[a];
+	}
+	for (b = 0; b < len_src; b++)
+	{
+		cat[len_dest] = src[b];
+		len_dest++;
+	}
+	cat[len_dest] = '\0';
+}
