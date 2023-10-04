c2dfdf43210180f36dce9ad2350427f2cce60084
oladev01 <lekanmojibola@gmail.com>
2023-10-04 18:46:24 +0100

    This is a project file

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * 
- * 
- * 
-*/
+ *free_grid - this program frees previously alloocated space for 2D array
+ * @grid: pointer to the 2D array
+ * @height: row section of the 2D array
+ * Return: null
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+	{
+		free(grid[a]);
+	}
+	free(grid);
+}
