b2dc966571b7196814e46a0d0715b80bb6e34dbe
oladev01 <lekanmojibola@gmail.com>
2023-10-05 17:55:27 +0100

    This is a project file

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this function returns no. of cents for a change
+ * @argc: number of arguments
+ * @argv: change to get the number of cents
+ * Return: the number of coins
+ */
+int main(int argc, char *argv[])
+{
+	int num, hold;
+
+	if (argc == 2)
+	{
+		num = atoi(argv[1]);
+		if (num < 0)
+		{
+			printf("0\n");
+		}
+		if (num >= 25)
+		{
+			hold = num / 25;
+			num = num % 25;
+		}
+		if (num >= 10)
+		{
+			hold = hold + (num / 10);
+			num = num % 10;
+		}
+		if (num >= 5)
+		{
+			hold = hold + (num / 5);
+			num = num % 5;
+		}
+		if (num >= 2)
+		{
+			hold = hold + (num / 2);
+			num = num % 2;
+		}
+		hold = hold + num;
+		printf("%d\n", hold);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
