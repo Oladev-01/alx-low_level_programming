9f46a11cc8470af196bf8fa89482e0c4f6cb37e3
oladev01 <lekanmojibola@gmail.com>
2023-10-06 20:21:52 +0100

    This is a project file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - the function allocates memory using malloc
+ * @b: the value we are alloctung space for
+ * Return: the pointer to the allocated memory space
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b * sizeof(unsigned int));
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This folder contains the codes for the more malloc project
