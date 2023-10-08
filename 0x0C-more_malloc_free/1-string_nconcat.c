7fde634a2fdf5bc70cdb189d5349924a6a133aac
olalekan <lekanmojibola@gmail.com>
2023-10-08 16:47:24 +0100

    This is a project file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s2 = "";
 	if (n >= len_2)
 	n = len_2;
-	str = malloc((len_1 + n + 1) * sizeof(char));
+	str = malloc((len_1 + n + 1));
 	if (str == NULL)
 	{
 		return (NULL);
