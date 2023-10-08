66f3df37a41b7b462cb0a3f5573fe2972ebb97aa
olalekan <lekanmojibola@gmail.com>
2023-10-08 18:19:27 +0100

    This is a project file

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,7 +13,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-		ptr = malloc(sizeof(int) * (max - min) + sizeof(int));
+		ptr = malloc((sizeof(int) * (max - min)) + sizeof(int));
 		if (ptr == NULL)
 			return (NULL);
 			while (min <= max)
