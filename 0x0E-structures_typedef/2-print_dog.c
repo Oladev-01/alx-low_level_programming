6ea998f92f2202b5187b2041502bc5db5064a3c6
olalekan <lekanmojibola@gmail.com>
2023-10-10 15:31:38 +0100

    This is a project file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - this function prints struct dog
+ * @d: this is a pointer to each elements of struct dog
+ *Return: void/null
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %lf\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
