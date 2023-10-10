8483eb4932de18e1ed7c93190c4b1c97e89ef822
olalekan <lekanmojibola@gmail.com>
2023-10-10 20:14:05 +0100

    This is a project file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this function prints the name of the
+ * file during compliltion of the program
+ *Return: 0 for success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
