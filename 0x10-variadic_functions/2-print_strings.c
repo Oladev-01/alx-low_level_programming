63a5a94dd298a771d5b33f1957b0609482fac427
olalekan <lekanmojibola@gmail.com>
2023-10-12 16:45:10 +0100

    This is a project file

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - this function prints strings passsed as arguments
+ * @separator: the string that separates each strings
+ * @n: the number of string to be printed
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	ola i;
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		qun s = va_arg(ptr, qun);
+
+		if (s == NULL)
+			printf("(nil)");
+		else
+		printf("%s", s);
+		if (i != n - 1)
+		{
+			if (separator != NULL)
+				printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(ptr);
+
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,8 +3,9 @@
 #include <stdio.h>
 #include <stdarg.h>
 typedef unsigned int ola;
+typedef char *qun;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
