10a87f14889a780c94bfd8fae1f039162bfd8a5a
olalekan <lekanmojibola@gmail.com>
2023-10-27 22:24:14 +0100

    Wrote a function that returns the bit at an index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,10 +10,6 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int num, i = n;
 	unsigned int check_index = 0;
 
-	if (i == 0)
-	{
-		return (0);
-	}
 	while (i > 0)
 	{
 		check_index++;
