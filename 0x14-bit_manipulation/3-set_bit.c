da38c453ae59c19ab52dcac14b78579644a812bd
olalekan <lekanmojibola@gmail.com>
2023-10-27 23:15:45 +0100

    Wrote a function that sets the bit at any index to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,8 +1,10 @@
 #include "main.h"
 #define BITS (sizeof(int) * 8)
 /**
- *
- *
+ * set_bit - this function sets the bit at any index to 1
+ *@n: this is the pointer to the number
+ *@index: this is the index of the bit to be set
+ *Return: 1 for success
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
