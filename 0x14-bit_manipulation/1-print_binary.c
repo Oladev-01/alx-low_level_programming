e864abf6832fcf9582b02c376a0103ffd4d49e41
olalekan <lekanmojibola@gmail.com>
2023-10-27 20:55:56 +0100

    Wrote a function that converts integer to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_binary - this function converts integers to binary
+ * @n: the integer to be converted
+ *Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i = 0;
+	unsigned int size = n, num;
+
+	if (size == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (size > 0)
+	{
+		i++;
+		size >>= 1;
+	}
+	i--;
+	while (i >= 0)
+	{
+		num = (n >> i) & 1;
+		_putchar(num ? '1' : '0');
+		i--;
+	}
+}
