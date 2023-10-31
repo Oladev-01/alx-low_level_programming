9b2eacfe3cffa1fe53ef4d812d34e61c034419d6
olalekan <lekanmojibola@gmail.com>
2023-10-31 13:15:46 +0100

    Wrote a function that appends string to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,8 +10,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	int file_op, len;
 	ssize_t num;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 	file_op = open(filename, O_WRONLY | O_APPEND);
 	if (file_op == -1)
 		return (-1);
