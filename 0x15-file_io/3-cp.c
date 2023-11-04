888bf66fb66986bfbc75fa9cd8c29969197939fa
olalekan <lekanmojibola@gmail.com>
2023-11-04 19:38:10 +0100

    This is a project file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,7 +36,7 @@ void cpy(const char *file_from, const char *file_to)
 	from_s = open(file_from, O_RDONLY);
 	if (from_s == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 	to_dest = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
