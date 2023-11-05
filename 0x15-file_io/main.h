1e6acaa3ea0e6c346f513d3d40310232811455d4
olalekan <lekanmojibola@gmail.com>
2023-11-05 08:36:53 +0100

    Made some changes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,4 +12,5 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void cpy(const char *file_from, const char *file_to);
 void handle_close(int from_s, int to_dest);
+void handle_no_read(const char *file_to);
 #endif
