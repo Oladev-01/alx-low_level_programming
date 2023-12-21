6092c8b34760c3e1605b1ab25c0ffd16d1cd4422
olalekan <lekanmojibola@gmail.com>
2023-12-21 08:33:25 +0100

    this code adds a new node at a given index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-	if (idx == 0 || *h = NULL)
+	if (idx == 0 || *h == NULL)
 	{
 		ptr->prev = new_node;
 		new_node->next = ptr;
