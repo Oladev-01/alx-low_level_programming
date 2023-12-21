49fc9d961b0dbf0878fb656f4e1ef870c9eb0977
olalekan <lekanmojibola@gmail.com>
2023-12-21 15:49:15 +0100

    This is a python code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,8 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0 || !(*h))
 	{
 		new_node->next = *h;
-		if (*h)
-			(*h)->prev = new_node;
+		(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
