e5cadbb0d59687e9ad65f71436eebd8f8d2460a3
olalekan <lekanmojibola@gmail.com>
2024-05-22 21:36:50 +0100

    jump search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - Searches for a value in a sorted array of integers using the
+ *               Jump search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: First index where value is located,
+ *  or -1 if not found or array is NULL
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t step, prev, i;
+
+	if (!array || size == 0)
+		return (-1);
+
+	step = sqrt(size);
+	prev = 0;
+
+	while (array[prev] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		if (step >= size)
+			break;
+		prev = step;
+		step += sqrt(size);
+		printf("this is step: %lu\n", step);
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n",
+	 prev - (size_t)sqrt(size), prev);
+
+	for (i = prev - (size_t)sqrt(size); i < size && i <= prev; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    size_t data = floor(sqrt(12));
+        printf("%ld\n", data);
+}
